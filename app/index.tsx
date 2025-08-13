9ee6dc396cfde523a5850742d0475d1bd672a33d
Hamzah Olaniyan <hamzaholaniyan88@gmail.com>
2025-08-13 10:24:32 +0100

    expo update linked

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+   return (
+      <View
+         style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "blue",
+         }}
+      >
+         <Text style={{ fontSize: 25, color: "white" }}>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+   );
 }
