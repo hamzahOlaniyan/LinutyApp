4017dd313b0c0bba03991dadff9c72207734cb13
Hamzah Olaniyan <hamzaholaniyan88@gmail.com>
2025-08-27 17:33:04 +0100

    version up date

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       ...config,
       name: "LinutyApp",
       slug: "LinutyApp",
-      version: "1.0.3",
+      version: "1.0.4",
       orientation: "portrait",
       icon: "./assets/images/icon.png",
       scheme: "linutyapp",
