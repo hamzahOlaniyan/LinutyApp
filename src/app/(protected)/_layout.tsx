3967c3aca0faa9071a7916a1656a702bbda6cbad
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2025-12-24 18:21:37 +0000

    fix: profile-user

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -8,9 +8,9 @@ export default function _ProtectedLayout() {
   const router = useRouter();
   const session = useAuthStore(s => s.session);
   const initialized = useAuthStore(s => s.initialized);
-  const me = useAuthStore(s => s.me);
+  // const me = useAuthStore(s => s.me);
 
-  const hasCompletedRegistration = !!me?.isProfileComplete;
+  // const hasCompletedRegistration = !!me?.isProfileComplete;
 
   if (!initialized) return null;
 
