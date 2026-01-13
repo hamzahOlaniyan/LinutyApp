e9f6294662ac4038877fb8a7a6a3b3aca4422c7c
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2026-01-13 02:35:09 +0000

    update: storage

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { SupportedStorage } from "@supabase/supabase-js";
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
@@ -8,34 +7,34 @@ let storage: SupportedStorage | undefined;
 if (typeof window !== "undefined") {
     storage = window.localStorage;
 }else{
-try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const AsyncStorage = require("@react-native-async-storage/async-storage").default;
-    storage = AsyncStorage;
-  } catch {
-    // During expo export / node environment: no storage available
-    storage = undefined;
-  }
+  try {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const AsyncStorage = require("@react-native-async-storage/async-storage").default;
+      storage = AsyncStorage;
+    } catch {
+      // During expo export / node environment: no storage available
+      storage = undefined;
+    }
 }
 
-const ExpoWebSecureStoreAdapter = {
-  getItem: (key: string) => {
-    // console.debug("getItem", { key })
-    return AsyncStorage.getItem(key)
-  },
-  setItem: (key: string, value: string) => {
-    return AsyncStorage.setItem(key, value)
-  },
-  removeItem: (key: string) => {
-    return AsyncStorage.removeItem(key)
-  },
-};
+// const ExpoWebSecureStoreAdapter = {
+//   getItem: (key: string) => {
+//     // console.debug("getItem", { key })
+//     return AsyncStorage.getItem(key)
+//   },
+//   setItem: (key: string, value: string) => {
+//     return AsyncStorage.setItem(key, value)
+//   },
+//   removeItem: (key: string) => {
+//     return AsyncStorage.removeItem(key)
+//   },
+// };
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
   {
     auth: {
-      storage: ExpoWebSecureStoreAdapter,
+      storage: storage,
       autoRefreshToken: true,
       persistSession: true,
       detectSessionInUrl: false,
