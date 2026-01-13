a8a6f332507a8da059781873e88e1f5b9b43fec3
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2026-01-13 02:34:47 +0000

    update: storage window update

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,55 +1,22 @@
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { createClient } from "@supabase/supabase-js";
-// import { deleteItemAsync, getItemAsync, setItemAsync } from 'expo-secure-store';
-// import "react-native-url-polyfill/auto";
-
-
-// const ExpoSecureStoreAdapter = {
-//   getItem: (key: string) => {
-//     console.debug("getItem", { key, getItemAsync })
-//     return getItemAsync(key)
-//   },
-//   setItem: (key: string, value: string) => {
-//     if (value.length > 2048) {
-//       console.warn('Value being stored in SecureStore is larger than 2048 bytes and it may not be stored successfully. In a future SDK version, this call may throw an error.')
-//     }
-//     return setItemAsync(key, value)
-//   },
-//   removeItem: (key: string) => {
-//     return deleteItemAsync(key)
-//   },
-// };
-
-
-// export const supabase = createClient(
-//   process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
-//   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
-//   {
-//     auth: {
-//       storage: AsyncStorage ,
-//       autoRefreshToken: true,
-//       persistSession: true,
-//       detectSessionInUrl: false,
-//     },
-//   },
-// );
-
-
-// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-// const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey,{
-//     auth: {
-//     storage: AsyncStorage,
-//     persistSession: true,
-//     autoRefreshToken: true,
-//     detectSessionInUrl: false,
-//   },
-// });
-
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { SupportedStorage } from "@supabase/supabase-js";
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
+
+let storage: SupportedStorage | undefined;
+
+if (typeof window !== "undefined") {
+    storage = window.localStorage;
+}else{
+try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const AsyncStorage = require("@react-native-async-storage/async-storage").default;
+    storage = AsyncStorage;
+  } catch {
+    // During expo export / node environment: no storage available
+    storage = undefined;
+  }
+}
 
 const ExpoWebSecureStoreAdapter = {
   getItem: (key: string) => {
